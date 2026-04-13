dc83292e7d9e27c78e869682425124dda3fa3e07
staslolpol <staspriho9@gmail.com>
2026-04-13 16:40:19 +0300

    Task03 complite

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -2,10 +2,9 @@
 
 bool mirror_vector(int array[], int length) {
 	bool result = true;
-	//int n = length / 2;
-	for (int i = 0; i < length ; i++)
+	for (int i = 0; i < length; i++)
 	{
-		if (array[i] != array[length - i]) {
+		if (array[i] != array[length - i - 1]) {
 			result = false;
 		}
 	}
--- Task03/main.cpp
+++ Task03/main.cpp
@@ -1,7 +1,7 @@
 #include "logic.h"
 
 int main() {
-	int array[]{ 1,2,3,4,5,5,4,3,2,1};
+	int array[]{ 1,2,3,4,5,5,4,4,2,1};
 	int length = 10;
 
 	string msg = mirror_vector(array, length) ? "The vector mirror"
